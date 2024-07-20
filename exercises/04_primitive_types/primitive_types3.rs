bfea880a8210d03268e7485e104ca541dd811625
Gocoder7 <gocoder7@gmail.com>
2024-07-20 11:13:15 +0900

    4-3

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,6 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a: [usize; 100] = core::array::from_fn(|i| i+1);
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
