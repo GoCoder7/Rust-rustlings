d2951afa8fce4967d2b919f458250b9228246e89
Gocoder7 <gocoder7@gmail.com>
2024-07-20 12:07:33 +0900

    5-2

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -3,7 +3,7 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
 
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
-        // the `output` vector.
+        output.push(element * 2);
     }
 
     output
@@ -24,7 +24,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+            element * 2
         })
         .collect()
 }
