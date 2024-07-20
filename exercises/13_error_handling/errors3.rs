0e851f315d58756aced66f390e6b3416edeb2d15
Gocoder7 <gocoder7@gmail.com>
2024-07-20 21:49:15 +0900

    13-3, 13-4

--- exercises/13_error_handling/errors3.rs
+++ exercises/13_error_handling/errors3.rs
@@ -2,7 +2,7 @@
 // `total_cost` function from the previous exercise. It's not working though!
 // Why not? What should we do to fix it?
 
-use std::num::ParseIntError;
+use std::{error::Error, num::ParseIntError};
 
 // Don't change this function.
 fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
@@ -15,7 +15,7 @@ fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
 
 // TODO: Fix the compiler error by changing the signature and body of the
 // `main` function.
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -24,8 +24,10 @@ fn main() {
 
     if cost > tokens {
         println!("You can't afford that many!");
+        Ok(())
     } else {
         tokens -= cost;
         println!("You now have {tokens} tokens.");
+        Ok(())
     }
 }
