367090defca6d2d3a2fb7496d1daee78e0648900
Gocoder7 <gocoder7@gmail.com>
2024-07-20 10:38:10 +0900

    quiz1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -10,7 +10,14 @@
 
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
-// fn calculate_price_of_apples(???) -> ??? { ??? }
+fn calculate_price_of_apples(quantity : i32) -> i32
+{
+    if quantity > 40 {
+        quantity
+    } else {
+        quantity * 2
+    }
+}
 
 fn main() {
     // You can optionally experiment here.
