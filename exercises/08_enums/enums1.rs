c55f92ea0593f99f20dcf86a068ce941c94895bf
Gocoder7 <gocoder7@gmail.com>
2024-07-20 15:27:45 +0900

    8-1

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,9 +1,15 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize,
+    Move,
+    Echo,
+    ChangeColor,
+    Quit
 }
 
 fn main() {
+    let message = Message::Resize;
     println!("{:?}", Message::Resize);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::Echo);
