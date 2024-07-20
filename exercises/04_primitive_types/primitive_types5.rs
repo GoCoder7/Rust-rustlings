1bd01f9cc1075ea3cf162a19c5746da3d00d791f
Gocoder7 <gocoder7@gmail.com>
2024-07-20 11:42:14 +0900

    4-5

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let cat = ("Furry McFurson", 3.5);
+    let (name, age) = ("Furry McFurson", 3.5);
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
     // let /* your pattern here */ = cat;
