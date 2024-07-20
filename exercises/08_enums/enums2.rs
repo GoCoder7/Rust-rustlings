edaa7e163593e808e61782890677d17fb77d4d29
Gocoder7 <gocoder7@gmail.com>
2024-07-20 15:32:38 +0900

    8-2

--- exercises/08_enums/enums2.rs
+++ exercises/08_enums/enums2.rs
@@ -9,6 +9,14 @@ struct Point {
 #[derive(Debug)]
 enum Message {
     // TODO: Define the different variants used below.
+    Resize {
+        width: i32,
+        height: i32,
+    },
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
 }
 
 impl Message {
